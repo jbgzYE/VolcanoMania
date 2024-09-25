afcedda7ba3111778b161df5b54b6d27a053acf6
jbgzYE <132840628+jbgzYE@users.noreply.github.com>
2024-09-25 17:35:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showAlert() {
+    alert("Boom! The volcano has erupted!");
+    document.getElementById("eruptionImage").innerHTML = '<img src="https://upload.wikimedia.org/wikipedia/commons/4/4d/Volcano_Mount_St._Helens.jpg" alt="Volcano Eruption">';
+}
